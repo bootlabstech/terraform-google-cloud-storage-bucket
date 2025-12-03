d40bc3a378afd9edcaba52257f6f2857f951aa50
Snekhapugal <SnekhaPuhazendhi>
2025-12-03 20:28:04 +0530

    versionchnage

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 ### Hashicorp Terraform Provider Configuration File ###
 terraform {
   required_version =">=0.13"
-
+  
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.41"
+      version = "4.1.0"
     }
   }
 }
