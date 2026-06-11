8e05457dd348913c7228cc951919d7c265401cfd
Bhargava Naidu <bhargava@bootlabstech.com>
2026-06-11 11:12:03 +0530

    updated new provider 6.41.0

--- provider.tf
+++ provider.tf
@@ -1,11 +1,10 @@
-### Hashicorp Terraform Provider Configuration File ###
 terraform {
-  required_version =">=0.13"
-  
+  required_version = ">=0.13"
+
   required_providers {
     google = {
+      version = "~> 6.41.0"
       source  = "hashicorp/google"
-      version = "4.1.0"
     }
   }
 }
