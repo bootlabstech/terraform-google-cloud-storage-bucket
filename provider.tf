993e4a55d38e37e6567924d923bdfe8a07254e7b
Bhargava Naidu <bhargava@bootlabstech.com>
2026-09-22 12:22:41 +0530

    Updated KMS key auto-fetch and API enablement

--- provider.tf
+++ provider.tf
@@ -1,29 +1,10 @@
-# terraform {
-#   required_version = ">=0.13"
-
-#   required_providers {
-#     google = {
-#       version = "4.1.0"
-#       source  = "hashicorp/google"
-#     }
-#   }
-# }
-
 terraform {
   required_version = ">=0.13"
 
   required_providers {
     google = {
+      version = "4.1.0"
       source  = "hashicorp/google"
-      version = "~> 6.41.0"
     }
   }
-}
-
-provider "google" {
-  project = "temptest-dev-419685" #"mpaas-sandbox-965286"
-  region  = "asia-south1"
-
-  # Uncomment this if you're using a service account key file:
-  # credentials = file("path/to/your-service-account.json")
 }
